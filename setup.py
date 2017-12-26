1bd51ed9633552979527418aadf5d2057246f124
Mattia <m.terenzi92@gmail.com>
2017-12-26 13:42:35 +0100

    improving cryptodice

--- setup.py
+++ setup.py
@@ -1,28 +1,15 @@
-import os
-import sys
-import platform
+from setuptools import setup, find_packages
 
-def do_tests():
-    exec(open("tests/test.py").read())
-
-def do_tests_with_env():
-    pass
-    #activate_this_file = ""
-    #execfile(activate_this_file, dict(__file__=activate_this_file))
-    #execfile("tests/test.py")
-
-print(sys.argv, sys.argv[1])
-
-if len(sys.argv) != 2:
-    raise ValueError('Too much arguments! Try with \'test\'')
-
-if str(sys.argv[1]) != 'test':
-    print(str(sys.argv[1]))
-    raise ValueError('Option not yet implemented. Try \'test\'')
-# Check if virtualenv is activated
-if hasattr(sys, 'real_prefix'):
-    do_tests()
-    sys.exit()
-else:
-    do_tests_with_env()
-    sys.exit()
+setup(
+    name="CryptoDice",
+    version="0.1",
+    packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
+    #entry_points={
+        #'console_scripts': [],
+        #'gui_scripts': []
+    #}
+    author="Mattia Terenzi",
+    author_email="mterenzi@openhedge.network",
+    description="A dice game based on smart contract.",
+    url="http://cryptodice.github.io"
+)
